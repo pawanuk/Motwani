2e41b457c887270c6f8989b2fce58f23565c3fa3
pawan <pawanmantra@gmail.com>
2019-04-16 16:02:28 +0100

    add boolean check

--- src/Motwani/add.java
+++ src/Motwani/add.java
@@ -1,10 +1,16 @@
 package Motwani;
 
 public class add {
-
+static boolean istrue(String s){
+	s="Pawan";
+	return false;
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("made some changes");
+add check=new add();
+System.out.println(check.istrue(""));
+
 	}
 
 }
